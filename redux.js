37f415b71e985d835134bc75d8d93d97408b1b7c
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-23 15:50:53 -0400

    action creators

--- redux.js
+++ redux.js
@@ -1,2 +1,32 @@
 // Redux Stephen Grider first example
 console.clear();
+
+// People dropping off a form (Action Creators)
+const createPolicy = (name, amount) => {
+    return { // Action (a form in our analogy)
+      type: 'CREATE_POLICY',
+      payload: {
+        name: name,
+        amount: amount
+      }
+    };
+  };
+  
+  const deletePolicy = (name) => {
+    return {
+      type: 'DELETE_POLICY',
+      payload: {
+        name: name
+      }
+    };
+  };
+  
+  const createClaim = (name, amountOfMoneyToCollect) => {
+    return {
+      type: 'CREATE_CLAIM',
+      payload: {
+        name: name,
+        amountOfMoneyToCollect: amountOfMoneyToCollect
+      }
+    };
+  };
